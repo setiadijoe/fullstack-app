04974e7b668a5fc7c16243fb5c676a7424573f38
setiadijoe <setiadi_y56@yahoo.co.id>
2020-07-02 15:54:17 +0700

    modified execute command

--- cmd/root.go
+++ cmd/root.go
@@ -33,24 +33,17 @@ var userLicense string
 var rootCmd = &cobra.Command{
 	Use:   "fullstack",
 	Short: "fullstack is a CRUD sample of blogs applications",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute() error {
+	rootCmd.AddCommand(serveCmd)
+	rootCmd.AddCommand(migrationCmd)
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
+	return nil
 }
 
 func init() {
--- cmd/serve.go
+++ cmd/serve.go
@@ -27,29 +27,18 @@ import (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "run the service",
 	Run: func(cmd *cobra.Command, args []string) {
 		app.Run()
 		fmt.Println("serve called")
 	},
 }
 
-func init() {
-	rootCmd.AddCommand(serveCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+var migrationCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "run a migration and seeder",
+	Run: func(cmd *cobra.Command, args []string) {
+		app.Migration()
+		fmt.Println("run migration")
+	},
 }
