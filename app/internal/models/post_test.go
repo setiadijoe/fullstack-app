101942561625f070b02a0c755b0ee020847b66ba
setiadijoe <setiadi_y56@yahoo.co.id>
2020-07-02 09:58:22 +0700

    create test on models

---
+++ app/internal/models/post_test.go
@@ -0,0 +1,127 @@
+package models_test
+
+import (
+	"log"
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql" // mysql dialect
+	"github.com/setiadijoe/fullstack/app/internal/models"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestFindAllPosts(t *testing.T) {
+
+	err := refreshUserAndPostTable()
+	if err != nil {
+		log.Fatalf("Error refreshing user and post table %v\n", err)
+	}
+	_, _, err = seedUsersAndPosts()
+	if err != nil {
+		log.Fatalf("Error seeding user and post  table %v\n", err)
+	}
+	posts, err := postInstance.FindAll(server.DB)
+	if err != nil {
+		t.Errorf("this is the error getting the posts: %v\n", err)
+		return
+	}
+	assert.Equal(t, len(*posts), 2)
+}
+
+func TestSavePost(t *testing.T) {
+
+	err := refreshUserAndPostTable()
+	if err != nil {
+		log.Fatalf("Error user and post refreshing table %v\n", err)
+	}
+
+	user, err := seedOneUser()
+	if err != nil {
+		log.Fatalf("Cannot seed user %v\n", err)
+	}
+
+	newPost := models.Post{
+		ID:       1,
+		Title:    "This is the title",
+		Content:  "This is the content",
+		AuthorID: user.ID,
+	}
+	savedPost, err := newPost.Save(server.DB)
+	if err != nil {
+		t.Errorf("this is the error getting the post: %v\n", err)
+		return
+	}
+	assert.Equal(t, newPost.ID, savedPost.ID)
+	assert.Equal(t, newPost.Title, savedPost.Title)
+	assert.Equal(t, newPost.Content, savedPost.Content)
+	assert.Equal(t, newPost.AuthorID, savedPost.AuthorID)
+
+}
+
+func TestGetPostByID(t *testing.T) {
+
+	err := refreshUserAndPostTable()
+	if err != nil {
+		log.Fatalf("Error refreshing user and post table: %v\n", err)
+	}
+	post, err := seedOneUserAndOnePost()
+	if err != nil {
+		log.Fatalf("Error Seeding table")
+	}
+	foundPost, err := postInstance.FindByID(server.DB, post.ID)
+	if err != nil {
+		t.Errorf("this is the error getting one user: %v\n", err)
+		return
+	}
+	assert.Equal(t, foundPost.ID, post.ID)
+	assert.Equal(t, foundPost.Title, post.Title)
+	assert.Equal(t, foundPost.Content, post.Content)
+}
+
+func TestUpdateAPost(t *testing.T) {
+
+	err := refreshUserAndPostTable()
+	if err != nil {
+		log.Fatalf("Error refreshing user and post table: %v\n", err)
+	}
+	post, err := seedOneUserAndOnePost()
+	if err != nil {
+		log.Fatalf("Error Seeding table")
+	}
+	postUpdate := models.Post{
+		ID:       1,
+		Title:    "modiUpdate",
+		Content:  "modiupdate@gmail.com",
+		AuthorID: post.AuthorID,
+	}
+	updatedPost, err := postUpdate.UpdateByID(server.DB)
+	if err != nil {
+		t.Errorf("this is the error updating the user: %v\n", err)
+		return
+	}
+	assert.Equal(t, updatedPost.ID, postUpdate.ID)
+	assert.Equal(t, updatedPost.Title, postUpdate.Title)
+	assert.Equal(t, updatedPost.Content, postUpdate.Content)
+	assert.Equal(t, updatedPost.AuthorID, postUpdate.AuthorID)
+}
+
+func TestDeleteAPost(t *testing.T) {
+
+	err := refreshUserAndPostTable()
+	if err != nil {
+		log.Fatalf("Error refreshing user and post table: %v\n", err)
+	}
+	post, err := seedOneUserAndOnePost()
+	if err != nil {
+		log.Fatalf("Error Seeding tables")
+	}
+	isDeleted, err := postInstance.DeleteByID(server.DB, post.ID, post.AuthorID)
+	if err != nil {
+		t.Errorf("this is the error updating the user: %v\n", err)
+		return
+	}
+	//one shows that the record has been deleted or:
+	// assert.Equal(t, int(isDeleted), 1)
+
+	//Can be done this way too
+	assert.Equal(t, isDeleted, isDeleted)
+}
---
+++ app/internal/models/user_test.go
@@ -0,0 +1,176 @@
+package models_test
+
+import (
+	"log"
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql" //mysql driver
+	"github.com/setiadijoe/fullstack/app/internal/models"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestFindAllUsers(t *testing.T) {
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = seedUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	users, err := userInstance.FindAll(server.DB)
+	if err != nil {
+		t.Errorf("this is the error getting the users: %v\n", err)
+		return
+	}
+	assert.Equal(t, len(*users), 2)
+}
+
+func TestLogin(t *testing.T) {
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = seedUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type testCase struct {
+		Email         string
+		Password      string
+		ExpectedError bool
+	}
+
+	samples := []*testCase{
+		{
+			Email:         "steven@gmail.com",
+			Password:      "password",
+			ExpectedError: false,
+		}, {
+			Email:         "lex",
+			Password:      "password",
+			ExpectedError: true,
+		}, {
+			Email:         "pet@gmail.com",
+			Password:      "expass",
+			ExpectedError: true,
+		},
+	}
+	for _, val := range samples {
+		result, err := userInstance.Login(server.DB, val.Email, val.Password)
+		if !val.ExpectedError {
+			if err != nil {
+				log.Fatal(err)
+			}
+			if result == nil {
+				log.Fatal("expected got token but nil")
+			}
+		}
+		if val.ExpectedError {
+			if err == nil {
+				log.Fatal("expected got error")
+			}
+		}
+	}
+
+}
+
+func TestSaveUser(t *testing.T) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	newUser := models.User{
+		ID:       1,
+		Email:    "test@gmail.com",
+		Nickname: "test",
+		Password: "password",
+	}
+	savedUser, err := newUser.Save(server.DB)
+	if err != nil {
+		t.Errorf("this is the error getting the users: %v\n", err)
+		return
+	}
+	assert.Equal(t, newUser.ID, savedUser.ID)
+	assert.Equal(t, newUser.Email, savedUser.Email)
+	assert.Equal(t, newUser.Nickname, savedUser.Nickname)
+}
+
+func TestGetUserByID(t *testing.T) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := seedOneUser()
+	if err != nil {
+		log.Fatalf("cannot seed users table: %v", err)
+	}
+	foundUser, err := userInstance.FindByID(server.DB, user.ID)
+	if err != nil {
+		t.Errorf("this is the error getting one user: %v\n", err)
+		return
+	}
+	assert.Equal(t, foundUser.ID, user.ID)
+	assert.Equal(t, foundUser.Email, user.Email)
+	assert.Equal(t, foundUser.Nickname, user.Nickname)
+}
+
+func TestUpdateAUser(t *testing.T) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := seedOneUser()
+	if err != nil {
+		log.Fatalf("Cannot seed user: %v\n", err)
+	}
+
+	userUpdate := models.User{
+		ID:       1,
+		Nickname: "modiUpdate",
+		Email:    "modiupdate@gmail.com",
+		Password: "password",
+	}
+	updatedUser, err := userUpdate.UpdateByID(server.DB, user.ID)
+	if err != nil {
+		t.Errorf("this is the error updating the user: %v\n", err)
+		return
+	}
+	assert.Equal(t, updatedUser.ID, userUpdate.ID)
+	assert.Equal(t, updatedUser.Email, userUpdate.Email)
+	assert.Equal(t, updatedUser.Nickname, userUpdate.Nickname)
+}
+
+func TestDeleteAUser(t *testing.T) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := seedOneUser()
+
+	if err != nil {
+		log.Fatalf("Cannot seed user: %v\n", err)
+	}
+
+	isDeleted, err := userInstance.DeleteByID(server.DB, user.ID)
+	if err != nil {
+		t.Errorf("this is the error updating the user: %v\n", err)
+		return
+	}
+	//one shows that the record has been deleted or:
+	// assert.Equal(t, int(isDeleted), 1)
+
+	//Can be done this way too
+	assert.Equal(t, isDeleted, isDeleted)
+}
